f8d49f3f81bc8011681e5a01eddb7c9409645624
[Jon Desserres] <[jondesserres@gmail.com]>
2023-12-07 13:21:51 -0700

    reworked hamburger menu to be more accessible to screen readers and adjusted the overall styling to help with the look of the site

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let toggleButton = document.getElementsByClassName('toggle-button')[0];
-let navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const menuToggle = document.querySelector('.menu-toggle');
+const siteNavigation = document.querySelector('.primary-navigation');
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+menuToggle.addEventListener('click', () => {
+  const isOpened = menuToggle.getAttribute('aria-expanded') === "true";
+  isOpened ? closeMenu() : openMenu();
+});
+
+function openMenu() {
+  menuToggle.setAttribute('aria-expanded', "true");
+  siteNavigation.setAttribute('data-state', "opened");
+}
+function closeMenu() {
+  menuToggle.setAttribute('aria-expanded', "false");
+  siteNavigation.setAttribute('data-state', "closing");
+
+  siteNavigation.addEventListener('animationend', () => {
+    siteNavigation.setAttribute('data-state', "closed");
+  }, {once: true})
+}
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
